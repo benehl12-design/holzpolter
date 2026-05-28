b430006637d403be9d613a2c1622393fcf5f6a8c
benehl12-design <benehl12@gmail.com>
2026-05-28 21:34:01 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const TILE_CACHE = 'lignum-tiles-v5';
 const SAT_CACHE  = 'lignum-sat-v5';
-const APP_VERSION = '4.5';
+const APP_VERSION = '4.7';
 const MAX_OSM = 600; const MAX_SAT = 1200;
 
 self.addEventListener('install',  () => self.skipWaiting());
